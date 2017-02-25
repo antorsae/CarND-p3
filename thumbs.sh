ec3e515a925dff8c622b078d1408785cce32ff2a
Andres Torrubia <antorsae@gmail.com>
2017-02-25 13:06:56 +0100

    Augmentations (JPG) and script

---
+++ thumbs.sh
@@ -0,0 +1,25 @@
+montage -label 'Original'    		train-0.jpg \
+		-label 'Flipped (F)' 		train-1.jpg \
+		-label 'Blurred (B)' 		train-2.jpg \
+		-label 'B+F' 				train-3.jpg \
+		-label 'Vertically Scaled (V)'			train-4.jpg \
+		-label 'V+F'				train-5.jpg \
+		-label 'Color jittered (C)' train-6.jpg \
+		-label 'C+F' 				train-7.jpg \
+		-label 'C+B'		 		train-8.jpg \
+		-label 'C+B+F' 				train-9.jpg \
+		-label 'C+V'				train-10.jpg \
+		-label 'C+V+F'				train-11.jpg \
+		-label 'Shadowed (S)'  		train-12.jpg \
+		-label 'S+F' 				train-13.jpg \
+		-label 'S+B'		 		train-14.jpg \
+		-label 'S+B+F' 				train-15.jpg \
+		-label 'S+V'				train-16.jpg \
+		-label 'S+V+F'				train-17.jpg \
+		-label 'S+C'				train-18.jpg \
+		-label 'S+C+F' 				train-19.jpg \
+		-label 'S+C+B'		 		train-20.jpg \
+		-label 'S+C+B+F' 			train-21.jpg \
+		-label 'S+C+V'				train-22.jpg \
+		-label 'S+C+V+F'			train-23.jpg \
+	-geometry 320x160+1+1 $1
